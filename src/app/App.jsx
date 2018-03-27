9f524369c4125a9b502f4c904d4b0ba2376062a3
zhangchengcheng <627395160@qq.com>
2018-03-27 18:53:07 +0800

    引入布局

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import {HashRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import {default as configureStore} from '../redux/reducer';
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+
 import {Home, About} from '../lib';
 
 class App extends Component {
@@ -13,24 +16,23 @@ class App extends Component {
         let store = configureStore();
         return (
             <Provider store={store}>
-                <div>
-                    <div className='personal-blog'>
-                        <h2>BLOG</h2>
-                    </div>
-                    <Router basename='/'>
-                        <div>
-                            <div>
-                                <Link to='/about'>about</Link>
-                                <Link to='/home'>home</Link>
-                            </div>
-                            <Switch>
-                                <Route exact path='/' component={Home}/>
-                                <Route exact path='/home' component={Home}/>
-                                <Route exact path='/about' component={About}/>
-                            </Switch>
-                        </div>
-                    </Router>
-                </div>
+                <Layout className='personal-blog'>
+                    <Sider>Sider</Sider>
+                    <Layout>
+                        <Header>Header</Header>
+                        <Content>
+                            <Router basename='/'>
+                                <div>
+                                    <Switch>
+                                        <Route exact path='/' component={Home}/>
+                                        <Route exact path='/home' component={Home}/>
+                                        <Route exact path='/about' component={About}/>
+                                    </Switch>
+                                </div>
+                            </Router>
+                        </Content>
+                    </Layout>
+                </Layout>
             </Provider>
         )
     }
